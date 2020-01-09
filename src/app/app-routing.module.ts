e45da2d518e773acb5fe09d819a282737dd650c6
xs <panov.alchemic@gmail.com>
2020-01-09 03:57:46 +0300

    login v2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,18 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {LoginComponent} from './components/login/login.component';
+import {LoginComponent} from './components/auth/login/login.component';
 import {AuthGuard} from './auth.guard';
 import {MatButtonModule, MatProgressSpinnerModule} from '@angular/material';
-import {VkHookComponent} from './components/vk-hook/vk-hook.component';
+import {VkHookComponent} from './components/auth/vk-hook/vk-hook.component';
+import {NoAccessComponent} from './components/auth/no-access/no-access.component';
 
 
 const routes: Routes = [
   {path: '', redirectTo: '/admin', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
   {path: 'vkhook', component: VkHookComponent},
-  {path: 'admin', canActivate: [AuthGuard], loadChildren: () => import('./components/admin/admin.module').then(mod => mod.AdminModule)}
+  {path: 'admin', canActivate: [AuthGuard], loadChildren: () => import('./components/admin/admin.module').then(mod => mod.AdminModule)},
+  {path: 'no-access', component: NoAccessComponent}
 ];
 
 @NgModule({
--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,43 +1,51 @@
 import {Injectable} from '@angular/core';
 import {CanActivate, Router} from '@angular/router';
 import {AuthVkService} from './auth-vk.service';
-import {environment} from '../environments/environment';
+import {VkResponse} from './components/auth/vk-hook/vk-hook.component';
+import {AdminService} from './services/admin.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
   canLogin = false;
-  private adminUsers = ['216737', '49593'];
+  adminSession;
 
   constructor(
     private auth: AuthVkService,
-    private router: Router
+    private router: Router,
+    private adminService: AdminService
   ) {
-    this.canLogin = !environment.production;
+    // this.canLogin = !environment.production;
   }
 
   logout() {
     this.canLogin = false;
+    this.adminSession = null;
+    localStorage.clear()
     this.router.navigateByUrl('/login');
   }
 
-  checkAccess(userId: string) {
-    if (this.adminUsers.includes(userId)) {
-      this.canLogin = true;
-      this.router.navigateByUrl('/admin/users');
-    } else {
-      this.router.navigateByUrl('/login');
-    }
+  checkAccess(vkResponse: VkResponse) {
+    this.adminService.getAdmins().subscribe(response => {
+        response.find((admin) => {
+          if (admin.user.vk_id === vkResponse.user_id) {
+            this.canLogin = true;
+            this.adminSession = admin;
+            this.router.navigateByUrl('/admin/users');
+            localStorage.setItem('token', vkResponse.access_token);
+          }
+        });
+      }
+    );
   }
 
   canActivate() {
-    if (this.canLogin) {
+    if (localStorage.getItem('token') && this.adminSession) {
       return true;
     } else {
       this.router.navigateByUrl('/login');
       return false;
     }
   }
-
 }
